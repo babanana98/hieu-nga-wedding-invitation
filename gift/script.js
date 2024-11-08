546955f8dc1d5078cf4e9ff72dba7edc493a5ded
babanana <quangminh1998x@gmail.com>
2024-11-08 16:31:11 +0700

    feat: change gift link

--- gift/script.js
+++ gift/script.js
@@ -3,16 +3,19 @@
 // Constant
 const GUEST_NAME_QUERY_PARAM = "guest";
 const GUEST_ROLE_QUERY_PARAM = "role";
+const HISTORY_QUERY_PARAM = "history";
 
 // JavaScript for handling image popup, zoom, and drag
 const invitationLinkElement = document.getElementById('invitationLink');
 const albumLinkElement = document.getElementById('albumLink');
+const giftBoxs = document.querySelectorAll('.gift-box');
 
 window.onload = function () {
   const guestName = getRequiredQueryParamOrElse(GUEST_NAME_QUERY_PARAM, "Quý khách");
   const guestRole = getRequiredQueryParamOrElse(GUEST_ROLE_QUERY_PARAM, "chúng tôi");
-  invitationLinkElement.href = invitationLinkElement.href + "?" + "role=" + guestRole + "&guest=" + guestName;
-  albumLinkElement.href = albumLinkElement.href + "?" + "role=" + guestRole + "&guest=" + guestName;
+  const history = getRequiredQueryParamOrElse(HISTORY_QUERY_PARAM, "../");
+  
+  setInvitationLinkAndBox(history, guestName, guestRole);
 };
 
 function getRequiredQueryParamOrElse(param, defaultVal) {
@@ -42,4 +45,29 @@ function showToast(message) {
   setTimeout(() => {
     toast.classList.remove("show");
   }, 2000);
+}
+
+function setInvitationLinkAndBox(history, guestName, guestRole) {
+  switch(history) {
+    case "groom":
+      invitationLinkElement.href = `../groom/?role=${guestRole}&guest=${guestName}`;
+      displayGiftBox(history);
+      break;
+    case "bride":
+      invitationLinkElement.href = `../bride/?role=${guestRole}&guest=${guestName}`;
+      displayGiftBox(history);
+      break;
+    default:
+      invitationLinkElement.href = `../?role=${guestRole}&guest=${guestName}`;
+      giftBoxs.forEach(box => box.classList.add("display-gift-box"));
+  }
+}
+
+function displayGiftBox(history) {
+  giftBoxs.forEach(box => {
+    if (box.getAttribute("name") == history) {
+      box.classList.add("display-gift-box");
+      return;
+    }
+  });
 }
